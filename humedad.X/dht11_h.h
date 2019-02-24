6b42666fb9591f8820a85152508489c19d3fb62d
María Mercedes Retolaza Reyna <jemcalzia@gmail.com>
2019-02-23 18:46:21 -0600

    Header & Main

---
+++ humedad.X/dht11_h.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   dht11_h.h
+ * Author: jmoli
+ *
+ * Created on 23 de febrero de 2019, 06:35 PM
+ */
+
+#ifndef DHT11_H_H
+#define	DHT11_H_H
+
+//Functions: 
+void dht11_begin(void);
+void dht11_check(void);
+uint8_t dht11_read(void);
+
+
+#endif	/* DHT11_H_H */
+
