1b18e4234ec0bffbbfb8492c57a7fa6591b92330
María Mercedes Retolaza Reyna <jemcalzia@gmail.com>
2019-02-24 14:23:48 -0600

    Humedad con interrupciones

--- humedad.X/nbproject/Makefile-default.mk
+++ humedad.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=humedad_main.c dht11_h.c
+SOURCEFILES_QUOTED_IF_SPACED=humedad_main.c dht11_h.c timer1.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/humedad_main.p1 ${OBJECTDIR}/dht11_h.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/humedad_main.p1.d ${OBJECTDIR}/dht11_h.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/humedad_main.p1 ${OBJECTDIR}/dht11_h.p1 ${OBJECTDIR}/timer1.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/humedad_main.p1.d ${OBJECTDIR}/dht11_h.p1.d ${OBJECTDIR}/timer1.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/humedad_main.p1 ${OBJECTDIR}/dht11_h.p1
+OBJECTFILES=${OBJECTDIR}/humedad_main.p1 ${OBJECTDIR}/dht11_h.p1 ${OBJECTDIR}/timer1.p1
 
 # Source Files
-SOURCEFILES=humedad_main.c dht11_h.c
+SOURCEFILES=humedad_main.c dht11_h.c timer1.c
 
 
 CFLAGS=
@@ -109,6 +109,14 @@ ${OBJECTDIR}/dht11_h.p1: dht11_h.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/dht11_h.d ${OBJECTDIR}/dht11_h.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/dht11_h.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/timer1.p1: timer1.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/timer1.p1.d 
+	@${RM} ${OBJECTDIR}/timer1.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/timer1.p1 timer1.c 
+	@-${MV} ${OBJECTDIR}/timer1.d ${OBJECTDIR}/timer1.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/timer1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
 ${OBJECTDIR}/humedad_main.p1: humedad_main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
@@ -125,6 +133,14 @@ ${OBJECTDIR}/dht11_h.p1: dht11_h.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/dht11_h.p1 dht11_h.c 
 	@-${MV} ${OBJECTDIR}/dht11_h.d ${OBJECTDIR}/dht11_h.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/dht11_h.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/timer1.p1: timer1.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/timer1.p1.d 
+	@${RM} ${OBJECTDIR}/timer1.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/timer1.p1 timer1.c 
+	@-${MV} ${OBJECTDIR}/timer1.d ${OBJECTDIR}/timer1.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/timer1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
